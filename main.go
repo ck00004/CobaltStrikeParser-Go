631d617d80d95fdfcbbbfad45e2fa691e56990f3
ck00004 <945178410@qq.com>
2022-10-27 15:26:50 +0800

    添加对Beacon文件的解析

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
 	"strings"
 	"sync"
@@ -18,6 +19,7 @@ var o = flag.String("o", "", "out file")
 var t = flag.Int("t", 30, "timeout. default:30")
 var br = flag.Int("br", 1, "thread,import file valid. default:1")
 var IsSave = flag.Bool("issave", false, "save not decrypted data to file ,Saved in the data directory. default:false")
+var BeaconFile = flag.String("beaconfile", "", "beacon config file path")
 
 func main() {
 	flag.Parse()
@@ -65,6 +67,16 @@ func main() {
 
 		close(ChanUrlList)
 		wg.Wait()
+	} else if *BeaconFile != "" {
+		filebuf, BeaconFileerr := os.OpenFile(*BeaconFile, os.O_RDWR, 0666)
+		if BeaconFileerr != nil {
+			fmt.Println("Open file error!", BeaconFileerr)
+			return
+		}
+		defer filebuf.Close()
+		BeaconBuf, _ := ioutil.ReadAll(filebuf)
+		got := beaconscan.Beacon_config(BeaconBuf)
+		fmt.Println(got)
 	} else {
 		if *o == "" {
 			beaconinfo, err := beaconscan.Beaconinit(*u, "", *t, *IsSave)
